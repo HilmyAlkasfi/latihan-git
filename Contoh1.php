92998a25cbcf67cbe33ae3afc25eb81c9459c938
HilmyAlkasfi <90598140+HilmyAlkasfi@users.noreply.github.com>
2021-10-22 17:56:30 +0700

    tugas 2
    menampilkan biodata

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan<h1>";
+        echo"nama saya hilmy
+            saya tinggal di Tasikmalaya
+            olah raga yang saya sukai adalah
+            bulutangkis";
+
+    }
+}
